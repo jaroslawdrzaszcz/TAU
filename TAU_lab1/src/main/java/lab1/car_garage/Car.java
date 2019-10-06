2b51cd7af83e44df2d886884e7478bcc3777da4c
Jaroslaw Drzaszcz <jaroslaw.drzasz@gmail.com>
2019-10-06 11:33:22 +0200

    create Car data

--- TAU_lab1/src/main/java/lab1/car_garage/Car.java
+++ TAU_lab1/src/main/java/lab1/car_garage/Car.java
@@ -1,4 +1,40 @@
 package lab1.car_garage;
 
 public class Car {
+    public int id;
+    public String registration;
+    public String brand;
+    public int owner_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
 }
