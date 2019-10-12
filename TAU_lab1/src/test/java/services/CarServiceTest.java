bb2524b74281f4dc1ce0d2f0ef7a34e304521aa1
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2019-10-12 20:49:28 +0200

    refactor after reading Lab 01 instruction

--- TAU_lab1/src/test/java/services/CarServiceTest.java
+++ TAU_lab1/src/test/java/services/CarServiceTest.java
@@ -1,4 +1,45 @@
 package services;
 
+import domain.Car;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(JUnit4.class)
 public class CarServiceTest {
+
+    CarService cars = new CarService(new ArrayList<Car>());
+
+    @Test
+    public void emptyCarRepositoryIsImplementedTest() {
+        assertNotNull(new CarServiceTest());
+    }
+
+    @Test
+    public void carRepositoryIsImplementedTest() {
+        assertNotNull(cars);
+    }
+
+    @Test
+    public void createMethodInCarRepositoryIsImplementedTest(){
+        assertNotNull(cars.create(new Car(1, "Test", "Test", 1)));
+    }
+
+    @Test
+    public void readMethodInCarRepositoryIsImplementedTest(){
+        assertNotNull(cars.read(0));
+    }
+    @Test
+    public void updateMethodInCarRepositoryIsImplementedTest(){
+        assertNotNull(cars.update(new Car()));
+    }
+//    @Test
+//    public void deleteMethodInCarRepositoryIsImplementedTest(){
+//        assertNotNull(cars.delete(0));
+//    }
+
 }
