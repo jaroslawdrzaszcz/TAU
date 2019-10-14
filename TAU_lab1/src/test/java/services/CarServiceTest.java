8ed43fec9c1236697852c9299c291ef9fba9d60b
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2019-10-14 20:54:15 +0200

    new CarService Tests

--- TAU_lab1/src/test/java/services/CarServiceTest.java
+++ TAU_lab1/src/test/java/services/CarServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class CarServiceTest {
@@ -49,6 +49,70 @@ public class CarServiceTest {
         testRepo.cars.add(car1);
 
         assertNotNull(testRepo.delete(0));
+    }
+
+    @Test
+    public void readAllMethodTest(){
+        int cars_number = 11;
+        for(int i = 0; i < cars_number; i++ ){
+            Integer iString = new Integer(i);
+            Car car = new Car(i, "GD 123" + iString.toString(), "Mada v" + iString.toString(), 100+i);
+            testRepo.cars.add(car);
+        }
+        assertEquals(cars_number, testRepo.cars.size());
+    }
+
+    @Test
+    public void createMethodTest(){
+        int cars_number = 11;
+        for(int i = 0; i < cars_number; i++ ){
+            Integer iString = new Integer(i);
+            testRepo.create(new Car(i, "GD 123" + iString.toString(), "Mada v" + iString.toString(), 100+i));
+        }
+        Car carToVerify = testRepo.read(5);
+        assertEquals(cars_number, testRepo.cars.size());
+        assertEquals(testRepo.cars.get(5), carToVerify);
+    }
+
+    @Test
+    public void readMethodTest(){
+        int cars_number = 11;
+        for(int i = 0; i < cars_number; i++ ){
+            Integer iString = new Integer(i);
+            Car car = new Car(i, "GD 123" + iString.toString(), "Mada v" + iString.toString(), 100+i);
+            testRepo.cars.add(car);
+        }
+        Car carToVerify = testRepo.read(5);
+        assertEquals(testRepo.cars.get(5), carToVerify);
+    }
+
+    @Test
+    public void updateMethodTest(){
+        int cars_number = 11;
+        for(int i = 0; i < cars_number; i++ ){
+            Integer iString = new Integer(i);
+            Car car = new Car(i, "GD 123" + iString.toString(), "Mada v" + iString.toString(), 100+i);
+            testRepo.cars.add(car);
+        }
+        Car newCar = new Car(10, "Test", "Test", 100);
+        testRepo.update(10, newCar);
+
+        assertEquals(cars_number, testRepo.cars.size());
+        assertEquals(testRepo.cars.get(10), newCar);
+    }
+
+    @Test
+    public void deleteMethodTest(){
+        int cars_number = 11;
+        for(int i = 0; i < cars_number; i++ ){
+            Integer iString = new Integer(i);
+            Car car = new Car(i, "GD 123" + iString.toString(), "Mada v" + iString.toString(), 100+i);
+            testRepo.cars.add(car);
+        }
+        testRepo.delete(1);
+        testRepo.delete(5);
+
+        assertEquals(cars_number - 2, testRepo.cars.size());
     }
 
 }
