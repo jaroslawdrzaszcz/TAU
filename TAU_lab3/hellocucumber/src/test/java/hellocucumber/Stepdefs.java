26f1b3c686e19a40fe402d3ad8fd266c9b78f459
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2019-11-20 22:11:46 +0100

    rest api jmeter

--- TAU_lab3/hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ TAU_lab3/hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -4,8 +4,37 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+class IsItFriday {
+
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
 
 public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
 
 }
