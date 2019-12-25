e4327d30cf4558eca67a3e5a606de9aacd62cc12
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2019-12-25 10:02:27 +0100

    Update calculator

--- TAU_lab6/calculate_triangle_quadrangle.py
+++ TAU_lab6/calculate_triangle_quadrangle.py
@@ -1,20 +1,30 @@
-def calculated_triangle(first_side, second_side, third_side):
-    if (first_side + second_side > third_side) or (first_side + third_side > second_side) or\
-            (second_side + third_side > first_side):
-        print("You can build the triangle with given sides.")
-    else:
-        print("You can't build the triangle with given sides.")
+class QuadTriCalculator:
+    def __init__(self, first_side=None, second_side=None, third_side=None, fourth_side=None):
+        self.first_side = first_side
+        self.second_side = second_side
+        self.third_side = third_side
+        self.fourth_side = fourth_side
 
+    def calculated_triangle(self):
+        if (self.first_side + self.second_side > self.third_side) \
+                or (self.first_side + self.third_side > self.second_side) \
+                or (self.second_side + self.third_side > self.first_side):
+            print("You can build the triangle with given sides.")
+        else:
+            print("You can't build the triangle with given sides.")
 
-def calculate_quadrangle(first_side, second_side, third_side, fourth_side):
-    if max(first_side, second_side, third_side, fourth_side) < (first_side + second_side + third_side + fourth_side -
-                                                                max(first_side, second_side, third_side, fourth_side)):
-        print("You can build the quadrangle with given sides.")
-    else:
-        print("You can't build the quadrangle with given sides.")
+    def calculate_quadrangle(self):
+        if max(self.first_side, self.second_side, self.third_side, self.fourth_side) \
+                < (self.first_side + self.second_side + self.third_side + self.fourth_side -
+                   max(self.first_side, self.second_side, self.third_side, self.fourth_side)):
+            print("You can build the quadrangle with given sides.")
+        else:
+            print("You can't build the quadrangle with given sides.")
 
 
 if __name__ == '__main__':
+    calculator = QuadTriCalculator()
+
     try:
         print("Enter float value of sides:")
         a = float(input("First side:"))
@@ -22,10 +32,10 @@ if __name__ == '__main__':
         c = float(input("Third side:"))
         key = input("If you want to build a triangle, press T, if you want to build a quadrangle, press Q:")
         if key == 'T':
-            calculated_triangle(a, b, c)
+            QuadTriCalculator(a, b, c).calculated_triangle()
         elif key == 'Q':
             d = float(input("Fourth side:"))
-            calculate_quadrangle(a, b, c, d)
+            QuadTriCalculator(a, b, c, d).calculate_quadrangle()
         else:
             print("Something went wrong")
     except ValueError:
